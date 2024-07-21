ae468369e1ed8932b062264376119ef7a9f11527
aamandabraun <aamandabraun@gmail.com>
2024-07-21 13:21:34 -0300

    new changes

--- tipos-variaves/src/TiposVariaves.java
+++ tipos-variaves/src/TiposVariaves.java
@@ -1,5 +1,13 @@
 public class TiposVariaves {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final int NUMERO = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
     }
 }
